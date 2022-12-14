f59a114b108b75be3f580434788102e1deceefbe
Alex-C <alexcook1613@gmail.com>
2022-12-14 18:49:43 -0500

    added SavedFighters page updates

---
+++ public/js/characterCreation.js
@@ -0,0 +1,46 @@
+// document.getElementById('buildCharacterBtn').onclick = function () {
+//     console.log('hello');
+//     location.href ='/public/pages/savedFighters.html';
+// };
+
+// fetch('http://localhost:3001/monsters')
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(monsters){
+//         let placeholder = document.querySelector('#data-output');
+//         let out = '';
+//         for (let monster of monsters) {
+//             out += `
+//             <tr>
+//                 <td>${monster.name}</td>
+//                 <td>${monster.cr}</td>
+//                 <td>${monster.xp}</td>
+//                 <td>${monster.ac}</td>
+//                 <td>${monster.hp}</td>
+//             </tr>
+//         `;
+//     }
+//     placeholder.innerHTML = out;
+// })
+
+// const character = require("../../routes/characterCreation")
+
+// const button = document.getElementById('btn');
+
+// button.addEventListener('click', async _ => {
+//     try {
+//         const response = await fetch('http://localhost:3001/')
+//     }
+// })
+
+// const saveCharacter = () => {
+        // fetch('/characters', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(character),
+    // });
+// console.log('hello');
+// }
